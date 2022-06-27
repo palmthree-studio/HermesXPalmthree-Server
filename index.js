4a1f027758c010ee1f4ced1ad0978639e566b8ff
PalmthreeStudio <palmthree@MacBook-Pro-de-PalmthreeStudio.local>
2022-06-27 02:14:17 +0200

    CommitStyle3

--- index.js
+++ index.js
@@ -55,6 +55,8 @@ app.get('/questions', (req, res) => {
   var question1 = questions[questionID1];
   var question2 = questions[questionID2];
   // Create and send the good object
+  console.log(question1);
+  console.log(question2);
   questions.push(question1,question2);
   console.log(questions);
   res.send(questions);
